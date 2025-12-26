656348dd87d360f1b62033773e4a758aa28c1097
Abhishek Sharma <49532014+abhishek021997@users.noreply.github.com>
2025-12-26 12:53:45 +0530

    Create service_monitor.sh for service monitoring
    Added a script to monitor critical services like sshd and nginx.

--- service_monitor.sh
+++ service_monitor.sh
@@ -1,1 +1,11 @@
-#
+#!/bin/bash
+# Monitor critical services
+
+SERVICES=("sshd" "nginx")
+
+for svc in "${SERVICES[@]}"; do
+    systemctl is-active --quiet "$svc"
+    if [ $? -ne 0 ]; then
+        echo "$svc is DOWN on $(hostname)"
+    fi
+done
